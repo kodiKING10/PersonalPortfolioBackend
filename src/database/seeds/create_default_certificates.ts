3718978bb74ca7697ea1529969aa6c916ed7bc74
Henrique Cavalcante <hencavalcante99@gmail.com>
2020-06-30 00:23:15 -0300

    populating database

---
+++ src/database/seeds/create_default_certificates.ts
@@ -0,0 +1,20 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex){
+    await knex('Certificate').insert([
+        { 
+            Institution: 'Senac', 
+            CourseName: "Computer Technician", 
+            Date: "2016 - 2017", 
+            Description: "Course finished in 2017 in one of the best institutions of  brazilian open professional education, covering the entire area of computer education.", 
+            Image: "http://localhost:3333/uploads/certificateSenac.png" 
+        },
+        { 
+            Institution: 'Centro Universitário das Faculdades Metropolitanas Unidas', 
+            CourseName: "Analysis and Systems Development", 
+            Date: "2018 - 2020", 
+            Description: "On going.", 
+            Image: "http://localhost:3333/uploads/certificateSenac.png" 
+        },
+    ]);
+}
